bce3c0a9445adae46ad786f1c2665d340f349462
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-05-28 19:49:49 -0400

    add checkpoints and refactor optimizers as parameters

--- train.py
+++ train.py
@@ -3,7 +3,8 @@
 import torch
 import torch.nn as nn
 from torch import optim
-from utils import device, MAX_LENGTH, SOS_token, EOS_token, time_since
+from utils import device, MAX_LENGTH, SOS_token, EOS_token, time_since,
+                                                save_checkpoint, load_checkpoint
 import time
 import random
 from visual import show_plot
@@ -64,19 +65,31 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer,
     return loss.item() / target_length
 
 
-def train_iters(encoder, decoder, training_pairs, n_iters, print_every=1000,
-                                            plot_every=100, learning_rate=0.01):
-    start = time.time()
-    plot_losses = []
-    print_loss_total = 0 # Reset every print_every
-    plot_loss_total = 0 # Reset every plot_every
+def train_iters(encoder, decoder, encoder_optim, decoder_optim,
+                training_pairs, n_iters, print_every=1000, plot_every=100,
+                save_every=1000, learning_rate=0.01):
 
-    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    checkpoint = load_checkpoint()
+    if checkpoint:
+        start_iter = checkpoint["iter"]
+        plot_losses = checkpoint["plot_losses"]
+        print_loss_total = checkpoint["print_loss_total"]
+        plot_loss_total = checkpoint["plot_loss_total"]
+        encoder.load_state_dict(checkpoint["encoder"])
+        decoder.load_state_dict(checkpoint["decoder"])
+        encoder_optim.load_state_dict(checkpoint["encoder_optim"])
+        decoder_optim.load_state_dict(checkpoint["decoder_optim"])
+
+    else:
+        start_iter = 1
+        plot_losses = []
+        print_loss_total = 0 # Reset every print_every
+        plot_loss_total = 0 # Reset every plot_every
 
     criterion = nn.NLLLoss()
 
-    for iter in range(1, n_iters + 1):
+    start = time.time()
+    for iter in range(start_iter, n_iters + 1):
         input_tensor, target_tensor = training_pairs[iter - 1]
 
         loss = train(input_tensor, target_tensor, encoder, decoder,
@@ -94,5 +107,17 @@ def train_iters(encoder, decoder, training_pairs, n_iters, print_every=1000,
             plot_loss_avg = plot_loss_total / plot_every
             plot_loss_total = 0
             plot_losses.append(plot_loss_avg)
+
+        if iter % save_every == 0:
+            save_checkpoint({
+                "iter": iter + 1,
+                "plot_losses": plot_losses,
+                "print_loss_total": print_loss_total,
+                "plot_loss_total": plot_loss_total,
+                "encoder": encoder.state_dict(),
+                "decoder": decoder.state_dict(),
+                "encoder_optimize": encoder_optimizer.state_dict(),
+                "decoder_optimizer": decoder_optimizer.state_dict(),
+            }, iter, save_every)
 
     show_plot(plot_losses)
