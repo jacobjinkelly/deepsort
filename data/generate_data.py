b7ef2ccff25ce7de410a740c45ec839203c7d946
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-05-28 18:07:25 -0400

    change file structure

--- data/generate_data.py
+++ data/generate_data.py
@@ -3,19 +3,17 @@
 import os
 import random
 
-path = "data/"
-
 def generate(size, max_val, min_length, max_length):
     """Generates <size> samples for the dataset,
     """
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    with open(path + "data.txt", mode='w') as file:
+    with open("data.txt", mode='w') as file:
         file.write("|".join([str(size), str(max_val), str(max_length)]) + "\n")
         for i in range(size):
             length = random.randint(min_length, max_length)
             lst = [0 for _ in range(length)]
             for i in range(length):
-                lst[i] = random.randint(0, max_val)
+                lst[i] = random.randint(2, max_val)
             srt = sorted(lst)
             file.write(str(lst) + "|" + str(srt) + "\n")
+
+# TODO: refactor using command line arguments
