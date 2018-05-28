3f203a5735ec1726192bde5cf6dd8544849a7046
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-05-28 18:07:51 -0400

    add set_max_length

--- utils.py
+++ utils.py
@@ -9,13 +9,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 SOS_token = 0
 EOS_token = 1
 
-MAX_LENGTH = 10
+MAX_LENGTH = 16
+
+def set_max_length(max_length):
+    MAX_LENGTH = max_length
 
 def as_minutes(s):
     m = math.floor(s / 60)
     s -= m * 60
     return '%dm %ds' % (m, s)
-
 
 def time_since(since, percent):
     now = time.time()
