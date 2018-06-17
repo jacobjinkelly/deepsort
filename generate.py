c754002f8814e82d285dbf614000ea47ce7ec4e9
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-06-17 18:13:13 -0400

    remove shadowing iterator bug

--- generate.py
+++ generate.py
@@ -1,23 +1,23 @@
 """This module generates the data for the sorting task.
 """
 import os
-import random
+import numpy as np
+
 from config.generate import get_config
+
 
 def generate(size, max_val, min_length, max_length):
     """Generates <size> samples for the dataset,
     """
     if not os.path.isdir("data"):
         os.mkdir("data")
-    with open("data/data.txt", mode='w') as file:
+    with open("data/train.txt", mode='w') as file:
         file.write("|".join([str(size), str(max_val), str(max_length)]) + "\n")
-        for i in range(size):
-            length = random.randint(min_length, max_length)
-            lst = [0 for _ in range(length)]
-            for i in range(length):
-                lst[i] = random.randint(2, max_val)
+        for j in range(size):
+            lst = np.random.randint(2, max_val, np.random.randint(min_length, max_length))
             srt = sorted(lst)
             file.write(str(lst) + "|" + str(srt) + "\n")
+
 
 if __name__ == "__main__":
     args, unparsed = get_config()
