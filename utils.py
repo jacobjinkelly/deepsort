dfdcea75b79e37ca154f945f351d08dfbad21ef0
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-06-17 18:14:10 -0400

    doc

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 """Miscellaneous utility functions.
 """
-import torch
-import time
 import math
 import os
+import time
+
+import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -12,43 +13,64 @@ EOS_token = 1
 
 MAX_LENGTH = 16
 
+
 def set_max_length(max_length):
+    """
+    Set the max length of an input sequence.
+    """
+    global MAX_LENGTH
     MAX_LENGTH = max_length
 
+
 def as_minutes(s):
+    """
+    Returns the number of seconds in minutes.
+    """
     m = math.floor(s / 60)
     s -= m * 60
     return '%dm %ds' % (m, s)
 
+
 def time_since(since, percent):
+    """
+    Return time since.
+    """
     now = time.time()
     s = now - since
-    es = s / (percent)
+    es = s / percent
     rs = es - s
     return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
 
-def save_checkpoint(state, iter, save_every):
-    old_file_name = "checkpoints/" + str(iter - 6 * save_every) + ".ckpt"
-    current_file_name = "checkpoints/" + str(iter) + ".ckpt"
+
+def save_checkpoint(state, iteration, save_every):
+    """
+    Save a checkpoint of the current model weights, deleting old ones.
+    """
+    old_file_name = "checkpoints/" + str(iteration - 6 * save_every) + ".ckpt"
+    current_file_name = "checkpoints/" + str(iteration) + ".ckpt"
     if os.path.isfile(old_file_name):
         os.remove(old_file_name)
     if not os.path.isdir("checkpoints"):
         os.mkdir("checkpoints")
     torch.save(state, current_file_name)
 
+
 def load_checkpoint():
+    """
+    Load the most recent (i.e. greatest number of iterations) checkpoint file.
+    """
     if os.path.isdir("checkpoints"):
         max_iter = -1
         max_iter_file = ""
         for file_name in os.listdir("checkpoints"):
             try:
-                iter = int(file_name.split(".")[0])
+                iteration = int(file_name.split(".")[0])
+                if file_name.endswith("ckpt") and iteration > max_iter:
+                    max_iter = iteration
+                    max_iter_file = file_name
             except ValueError:
                 print("A file other than a checkpoint appears to be in the " +
-                        "<checkpoints> folder; please remove it")
-            if file_name.endswith("ckpt") and iter > max_iter:
-                max_iter = iter
-                max_iter_file = file_name
+                      "<checkpoints> folder; please remove it")
         if max_iter > 0:
             print("Loading checkpoint file...")
             return torch.load("checkpoints/" + max_iter_file)
