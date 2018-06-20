37cc5bab4c9b01446b484c3320f5e075737cdcb6
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-06-20 18:41:32 -0400

    add ewc option

--- config/generate.py
+++ config/generate.py
@@ -4,10 +4,12 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--name", type=str, help="the name of the dataset (e.g. train, val, test)")
 parser.add_argument("--size", type=int, help="size of the dataset to generate")
 parser.add_argument("--max_val", type=int, help="the maximum value in an array")
 parser.add_argument("--min_length", type=int, help="the min size of array")
 parser.add_argument("--max_length", type=int, help="the max size of array")
+parser.add_argument("--ewc", action="store_true", help="generate sequence of datasets for ewc")
 
 
 def get_config():
--- generate.py
+++ generate.py
@@ -6,16 +6,24 @@ import numpy as np
 from config.generate import get_config
 
 
-def generate(size, max_val, min_length, max_length):
+def generate(name, size, max_val, min_length, max_length, ewc):
     """Generates <size> samples for the dataset,
     """
     if not os.path.isdir("data"):
         os.mkdir("data")
-    with open("data/train.txt", mode='w') as file:
+    with open("data/" + name + ".txt", mode='w') as file:
         file.write("|".join([str(size), str(max_val), str(max_length)]) + "\n")
-        for j in range(size):
-            if j % 10000 == 0:
-                print(j)
+        if ewc:
+            for length in range(2, max_length + 1):
+                for i in range(size):
+                    if i % 10000 == 0:
+                        print(length, i)
+                    lst = list(np.random.randint(2, max_val, length))
+                    srt = sorted(lst)
+                    file.write(str(lst) + "|" + str(srt) + "\n")
+        for i in range(size):
+            if i % 10000 == 0:
+                print(i)
             lst = list(np.random.randint(2, max_val, np.random.randint(min_length, max_length)))
             srt = sorted(lst)
             file.write(str(lst) + "|" + str(srt) + "\n")
@@ -23,4 +31,4 @@ def generate(size, max_val, min_length, max_length):
 
 if __name__ == "__main__":
     args, unparsed = get_config()
-    generate(args.size, args.max_val, args.min_length, args.max_length)
+    generate(args.name, args.size, args.max_val, args.min_length, args.max_length, args.ewc)
